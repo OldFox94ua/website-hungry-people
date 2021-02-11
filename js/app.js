1031bb89de8e919cb248f3b4abab4b2231d2d110
Vadim <mitai3627@gmail.com>
2021-02-11 10:41:49 +0200

    fix:adapter slider
    added adaptive view for the slider when the screen is reduced, the number of displayed images decreases, also fixing some small errors in the indentation of the blocks

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const testSlider = new Swiper('.test-slider',{
+const testSlider = new Swiper('.test-slider', {
     slidesPerView: 4,
     spaceBetween: 0,
     loop: true,
@@ -6,9 +6,28 @@ const testSlider = new Swiper('.test-slider',{
         nextEl: '.test-slider-next',
         prevEl: '.test-slider-prev'
     },
-    // autoplay: {
-    //     delay: 2000
-    // }
+    autoplay: {
+        delay: 2000
+    },
+    slidesPerView: 1,
+    spaceBetween: 5,
+    // Responsive breakpoints
+    breakpoints: {
+        575: {
+            slidesPerView: 2,
+            spaceBetween: 0
+        },
+        767: {
+            slidesPerView: 3,
+            spaceBetween: 0
+        },
+        991: {
+            slidesPerView: 4,
+            spaceBetween: 0
+        }
+    }
+
+
 });
 
 const specialitesSlider = new Swiper('.specialties-slider', {
@@ -20,6 +39,6 @@ const specialitesSlider = new Swiper('.specialties-slider', {
         clickable: true,
         bulletClass: 'slider-bullet',
         bulletActiveClass: 'slider-bullet-active'
-      },
+    },
 })
 
